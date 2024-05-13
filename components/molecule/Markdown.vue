ee79f31a7f7c17f58298d75c5f6acfff0f19c382
Ambrizal Suryadinata <ambrizals@outlook.co.id>
2024-05-13 13:45:57 +0700

    fix: blog render

---
+++ components/molecule/Markdown.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-html="markdown.render(source)" />
+</template>
+
+<script setup lang="ts">
+import '~/assets/hljs.css'
+
+import MarkdownIt from "markdown-it";
+import MarkdownItAnchor from "markdown-it-anchor";
+import MarkdownItHighlightjs from "markdown-it-highlightjs";
+import MarkdownItNamedCodeBlock from 'markdown-it-named-code-blocks'
+//@ts-ignore
+import MarkdownItToc from 'markdown-it-table-of-contents'
+import MarkdownItTocDoneRight from 'markdown-it-toc-done-right'
+
+const markdown = new MarkdownIt()
+  .use(MarkdownItAnchor, {
+    permalink: MarkdownItAnchor.permalink.linkInsideHeader({
+      symbol: `
+        <svg data-v-4fa0a2a1="" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="icon text-lg" width="1em" height="1em" viewBox="0 0 16 16"><g fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"><path d="M8.914 6.025a.75.75 0 0 1 1.06 0a3.5 3.5 0 0 1 0 4.95l-2 2a3.5 3.5 0 0 1-5.396-4.402a.75.75 0 0 1 1.251.827a2 2 0 0 0 3.085 2.514l2-2a2 2 0 0 0 0-2.828a.75.75 0 0 1 0-1.06"></path><path d="M7.086 9.975a.75.75 0 0 1-1.06 0a3.5 3.5 0 0 1 0-4.95l2-2a3.5 3.5 0 0 1 5.396 4.402a.75.75 0 0 1-1.251-.827a2 2 0 0 0-3.085-2.514l-2 2a2 2 0 0 0 0 2.828a.75.75 0 0 1 0 1.06"></path></g></svg>
+      `,
+      placement: 'before',
+      class: 'test'
+    })
+  })
+  .use(MarkdownItHighlightjs)
+  .use(MarkdownItNamedCodeBlock)
+  .use(MarkdownItTocDoneRight, {})
+
+defineProps({
+  source: {
+    type: String,
+    default: ""
+  }
+});
+</script>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import '~/assets/blog.css'
 const { params } = useRoute()
 const { $client } = useNuxtApp()
 
-const { data, pending } = await $client.blog.read.useQuery(params.slug.toString())
+const { data, status } = await $client.blog.read.useQuery(params.slug.toString())
 </script>
 
 <template>
@@ -20,15 +21,8 @@ const { data, pending } = await $client.blog.read.useQuery(params.slug.toString(
         <div class="bg-gray-200 dark:bg-gray-800 px-4 py-2"><UIcon name="i-heroicons-link-16-solid" dynamic class="text-lg"/></div>
       </div>
     </div>
-    <div class="py-4 px-8">
-      <h2 class="text-4xl my-4 font-bold">Testing Heading 1</h2>
-      <p class="leading-5 mb-2">Elit pariatur duis enim culpa sunt labore magna ad. Quis officia non in laborum culpa est culpa ad velit ut anim commodo. Ipsum proident aute labore tempor nulla cupidatat anim reprehenderit reprehenderit dolore. Minim quis aliqua commodo id cillum cillum deserunt minim eu consectetur nisi ut. Laboris consequat elit dolore aliquip laboris excepteur commodo consequat id id in labore tempor. Irure consequat sunt officia consequat fugiat eu consequat velit dolor enim excepteur proident.</p>
-      <h2 class="text-4xl mb-4 font-semibold">Testing Heading 2</h2>
-      <p class="leading-5 mb-2">Elit pariatur duis enim culpa sunt labore magna ad. Quis officia non in laborum culpa est culpa ad velit ut anim commodo. Ipsum proident aute labore tempor nulla cupidatat anim reprehenderit reprehenderit dolore. Minim quis aliqua commodo id cillum cillum deserunt minim eu consectetur nisi ut. Laboris consequat elit dolore aliquip laboris excepteur commodo consequat id id in labore tempor. Irure consequat sunt officia consequat fugiat eu consequat velit dolor enim excepteur proident.</p>
-      <h3 class="text-3xl mb-4">Testing Heading 3</h3>
-      <p class="leading-5 mb-2">Elit pariatur duis enim culpa sunt labore magna ad. Quis officia non in laborum culpa est culpa ad velit ut anim commodo. Ipsum proident aute labore tempor nulla cupidatat anim reprehenderit reprehenderit dolore. Minim quis aliqua commodo id cillum cillum deserunt minim eu consectetur nisi ut. Laboris consequat elit dolore aliquip laboris excepteur commodo consequat id id in labore tempor. Irure consequat sunt officia consequat fugiat eu consequat velit dolor enim excepteur proident.</p>
-      <h3 class="text-2xl mb-2">Testing Heading 4</h3>
-      <p class="leading-5 mb-2">Elit pariatur duis enim culpa sunt labore magna ad. Quis officia non in laborum culpa est culpa ad velit ut anim commodo. Ipsum proident aute labore tempor nulla cupidatat anim reprehenderit reprehenderit dolore. Minim quis aliqua commodo id cillum cillum deserunt minim eu consectetur nisi ut. Laboris consequat elit dolore aliquip laboris excepteur commodo consequat id id in labore tempor. Irure consequat sunt officia consequat fugiat eu consequat velit dolor enim excepteur proident.</p>
+    <div v-if="status === 'success'" class="py-4 px-8 blog">
+      <Markdown :source="data!.content!" />
     </div>
   </div>
 </template>
