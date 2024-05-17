9bc6d141f8ec2fd1e14c96de47822ae3a10b2eb3
Ambrizal Suryadinata <ambrizals@outlook.co.id>
2024-05-17 17:19:44 +0700

    feature: drag & drop image

---
+++ components/molecule/PrompDragDrop.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="flex flex-col justify-center items-center w-full text-sm text-center cursor-pointer border-2 hover:border-solid border-dashed border-gray-800 dark:border-gray-400 bg-white dark:bg-gray-600 p-2 rounded-md"
+  >
+    <div>
+      <UIcon name="i-heroicons-cloud-arrow-down-solid" class="text-[48px]" />
+      <div>Click or drop an image to change cover</div>
+    </div>
+  </div>
+</template>
--- pages/dap/blog/[...slug].client.vue
+++ pages/dap/blog/[...slug].client.vue
@@ -22,11 +22,14 @@ const { params } = useRoute()
 const { $client } = useNuxtApp()
 
 const articleEntry = ref<BlogReadOutputAPI>()
+const fileInput = ref<HTMLInputElement | null>(null);
 const content = ref('')
 const title = ref('')
 const description = ref('')
 const isPublished = ref(false)
 const editorRef = ref<ExposeParam>();
+const fileCover = ref<File | null>(null)
+const filePreview = ref<string | null>(null)
 
 const autoSave = ref(false)
 
@@ -79,10 +82,81 @@ function save() {
     })
   }
 }
+
+function onDragOver(event: DragEvent) {
+  event.preventDefault();
+};
+
+function onDragLeave(event: DragEvent) {
+  event.preventDefault();
+};
+
+function validateFile(file: File) {
+  const validTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/bmp'];
+  return validTypes.includes(file.type);
+};
+
+function onDrop(event: DragEvent) {
+  const droppedFiles = event.dataTransfer?.files;
+  if (droppedFiles && droppedFiles.length > 0) {
+    const droppedFile = droppedFiles[0];
+    if (validateFile(droppedFile)) {
+      previewImage(droppedFile)
+      fileCover.value = droppedFile;
+    } else {
+      toast.add({
+        color: 'red',
+        title: 'Error validation',
+        description: 'Hanya menerima file gambar'
+      })
+    }
+  }
+};
+
+const onFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  const selectedFiles = target.files;
+  if (selectedFiles && selectedFiles.length > 0) {
+    const selectedFile = selectedFiles[0];
+    if (validateFile(selectedFile)) {
+      previewImage(selectedFile)
+      fileCover.value = selectedFile;
+    } else {
+      toast.add({
+        color: 'red',
+        title: 'Error validation',
+        description: 'Hanya menerima file gambar'
+      })
+    }
+  }
+};
+
+function onDragClick() {
+  fileInput.value?.click();
+};
+
+function previewImage(file: File) {
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    filePreview.value = e.target?.result as string;
+  };
+  reader.readAsDataURL(file);
+};
+
+watch(fileCover, (value) => {
+  console.log(value)
+})
 </script>
 
 <template>
   <div class="h-screen flex flex-col gap-2 px-4 py-2">
+    <input
+      type="file"
+      ref="fileInput"
+      @change="onFileChange"
+      class="hidden"
+      accept="image/*"
+    />
     <div class="flex gap-2 items-center">
       <UInput v-model="title" class="flex-1" placeholder="Silakan masukkan judul artikel" size="lg" />
       <div class="flex gap-2 w-fit">
@@ -99,6 +173,48 @@ function save() {
         <UButton block variant="outline" :to="`/blog/${articleEntry?.slug}`" target="_blank">Preview Article</UButton>
         <div class="h-2" />
         <UButton icon="i-heroicons-trash" block color="red" variant="outline">Delete Article</UButton>
+
+        <div class="h-4" />
+
+        <div class="relative group">
+          <div v-if="articleEntry?.thumbnail && filePreview === null" class="relative"
+            @dragover.prevent="onDragOver"
+            @dragleave.prevent="onDragLeave"
+            @drop.prevent="onDrop"
+            @click="onDragClick()"          
+          >
+            <PrompDragDrop
+              @click="onDragClick()"
+              class="absolute top-0 h-full bg-opacity-90 group-hover:block hidden"
+            />   
+            <NuxtImg class="w-full rounded-md" provider="localEnhance" :src="`cover/${articleEntry?.thumbnail}`" />
+          </div>
+          <!-- <div v-if="articleEntry?.thumbnail" class="rounded-md bg-gray-500 flex-col justify-center items-center text-center p-4 cursor-pointer text-white bg-opacity-80 w-full h-full absolute top-0 group-hover:flex hidden">
+            <UIcon name="i-heroicons-cloud-arrow-down-solid" class="text-[48px]" />
+            <div>Click or drop an image to change cover</div>
+          </div> -->
+          <!-- Upload drag & drop -->
+          <PrompDragDrop
+            v-else-if="(articleEntry?.thumbnail === undefined || articleEntry?.thumbnail === null) && filePreview === null"
+            @dragover.prevent="onDragOver"
+            @dragleave.prevent="onDragLeave"
+            @drop.prevent="onDrop"
+            @click="onDragClick()"
+          />
+
+          <div v-else-if="filePreview !== null"
+            @dragover.prevent="onDragOver"
+            @dragleave.prevent="onDragLeave"
+            @drop.prevent="onDrop"          
+            class="rounded-md overflow-hidden group relative"
+          >
+            <PrompDragDrop
+              @click="onDragClick()"
+              class="absolute top-0 h-full bg-opacity-90 group-hover:block hidden"
+            />            
+            <img :src="filePreview" alt="cover" />
+          </div>
+        </div>
 
         <div class="h-4" />
 
@@ -125,27 +241,7 @@ function save() {
             <CompTitle label="Deskripsi" size="base" />
           </template>          
           <UTextarea v-model="description" variant="outline" color="primary" size="lg" :rows="7" />
-        </UCard>
-
-        <div class="h-4" />
-
-        <UCard :ui="{
-          body: {
-            base: '',
-            background: '',
-            padding: ''
-          }
-        }">
-          <template #header>
-            <CompTitle label="Cover" size="base" />
-          </template>
-          <div class="relative group">
-            <div class="bg-gray-500 justify-center items-center text-center p-4 cursor-pointer text-white bg-opacity-80 w-full h-full absolute top-0 group-hover:flex hidden">
-              Click or drop an image to change cover
-            </div>
-            <NuxtImg class="w-full" provider="localEnhance" :src="`cover/${articleEntry?.thumbnail}`" />
-          </div>
-        </UCard>        
+        </UCard>   
       </div>
     </div>
   </div>
