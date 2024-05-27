5919879d14f481b8eb1299ce600cdf54de663d95
Ambrizal Suryadinata <ambrizals@outlook.co.id>
2024-05-27 10:28:13 +0700

    feat: markdown theme

--- components/molecule/Markdown.vue
+++ components/molecule/Markdown.vue
@@ -14,7 +14,7 @@ const markdown = new MarkdownIt()
         <svg data-v-4fa0a2a1="" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="icon text-lg" width="1em" height="1em" viewBox="0 0 16 16"><g fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"><path d="M8.914 6.025a.75.75 0 0 1 1.06 0a3.5 3.5 0 0 1 0 4.95l-2 2a3.5 3.5 0 0 1-5.396-4.402a.75.75 0 0 1 1.251.827a2 2 0 0 0 3.085 2.514l2-2a2 2 0 0 0 0-2.828a.75.75 0 0 1 0-1.06"></path><path d="M7.086 9.975a.75.75 0 0 1-1.06 0a3.5 3.5 0 0 1 0-4.95l2-2a3.5 3.5 0 0 1 5.396 4.402a.75.75 0 0 1-1.251-.827a2 2 0 0 0-3.085-2.514l-2 2a2 2 0 0 0 0 2.828a.75.75 0 0 1 0 1.06"></path></g></svg>
       `,
       placement: 'before',
-      class: 'test'
+      class: 'toc-anchor'
     })
   })
   .use(MarkdownItHighlightjs)
